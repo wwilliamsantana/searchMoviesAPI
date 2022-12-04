e0fc8b1beecbfd3adaea69455b07444ec772ba6e
William Silva <euwilliamsantana@gmail.com>
2022-12-04 11:40:22 -0300

    Inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+const url = import.meta.env.VITE_API
+const key = import.meta.env.VITE_API_KEY
+
+export function App() {
+  console.log(url, key)
+  return (
+   <div>
+    
+   </div>
+  )
+}
+
