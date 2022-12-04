edbb05e200884731f4952a3aefedf1c82dec4e00
William Silva <euwilliamsantana@gmail.com>
2022-12-04 12:49:44 -0300

    Estrutura React-Router-DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/defaultTheme'
 
-const url = import.meta.env.VITE_API
-const key = import.meta.env.VITE_API_KEY
+// const url = import.meta.env.VITE_API
+// const key = import.meta.env.VITE_API_KEY
 
 export function App() {
-  const [movies, setMovies] = useState([])
+  // const [movies, setMovies] = useState([])
 
-  async function getMoviesTopRated() {
-    const response = await axios(`${url}top_rated`, {
-      params: {
-        api_key: key,
-        language: 'pt-BR',
-      },
-    })
+  // async function getMoviesTopRated() {
+  //   const response = await axios(`${url}top_rated`, {
+  //     params: {
+  //       api_key: key,
+  //       language: 'pt-BR',
+  //     },
+  //   })
 
-    const data = await response.data
-    setMovies(data.results)
-  }
+  //   const data = await response.data
+  //   setMovies(data.results)
+  // }
 
-  useEffect(() => {
-    getMoviesTopRated()
-  }, [])
+  // useEffect(() => {
+  //   getMoviesTopRated()
+  // }, [])
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>Hello World</div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
